174d18d04eb81fafdd3a5a3f2b553cc111b6fa94
benalloway <benalloway@gmail.com>
2018-07-20 14:17:54 -0400

    added NaturalLanguage model, customized some things, and registered the models in the Admin site

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, NaturalLanguage, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(NaturalLanguage)
+
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -21,6 +21,23 @@ class Genre(models.Model):
 
 
 ################
+# NaturalLanguage 
+################
+class NaturalLanguage(models.Model):
+	"""
+	Model representing the natural language of the book (e.g. English, French, Japanese etc.)
+	"""
+
+	name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French etc.)")
+
+	def __str__(self):
+		"""
+		String for representing the Model object (in Admin site etc.)
+		"""
+		return self.name
+
+
+################
 # Book 
 ################
 class Book(models.Model):
@@ -29,14 +46,19 @@ class Book(models.Model):
 	"""
 
 	title = models.CharField(max_length=200)
-	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-	# Foreign Key used because book can only have one author, but authors can have multiple books
-	# Author as a string rather than object bc/ it hasn't been declared yet in the file.
 	summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
 	isbn = models.CharField("ISBN", max_length=13, help_text="13 Character <a href='https://www.isbn-international.org/content/what-isbn'>ISBN number</a>")
-	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+	# Foreign Key used because Book can only have one natural language.
+	language = models.ForeignKey(NaturalLanguage, on_delete=models.SET_NULL, null=True)
+
+	# Author as a string rather than object bc/ it hasn't been declared yet in the file.
+	# Foreign Key used because book can only have one author, but authors can have multiple books
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
 	# ManyToManyField used bc/ genre can contain many books. Books can cover many genres.
 	# Genre class has already been defined so we can specify the object above.
+	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")	
 
 	def __str__(self):
 		"""
@@ -52,28 +74,29 @@ class Book(models.Model):
 
 		return reverse('book-detail', args=[str(self.id)])
 
-import uuid # Required for unique book instances
 
 ################
 # BookInstance 
 ################
+import uuid # Required for unique book instances
+
+
 class BookInstance(models.Model):
 	"""
 	Model representing a a specific copy of a book (i.e that can be borrowed from the library).
 	"""
 
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-	book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-	imprint = models.CharField(max_length=200)
-	due_back = models.DateField(null=True, blank=True)
-
+	# List of book's loan statuses, default to Maintenance when book is created (until it is stocked)
 	LOAN_STATUS = (
 		('m', 'Maintenance'),
 		('o', 'On Loan'),
 		('a', 'Available'),
 		('r', 'Reserved'),
 	)
-
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+	book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+	imprint = models.CharField(max_length=200)
+	due_back = models.DateField(null=True, blank=True)
 	status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text="Book availability")
 
 	class Meta:
