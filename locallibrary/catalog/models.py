3742561994dfc2f7fb339f41194c139f3ec09782
benalloway <benalloway@gmail.com>
2018-07-20 12:18:14 -0400

    added in models: Genre, Book,

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse #Used to generate URLs by reversing the URL patt
 ################
 # Genre 
 ################
-class Ganre(models.Model):
+class Genre(models.Model):
 	"""
 	Model representing a book genre (e.g. Science Fiction, Non Fiction, Military History)
 	"""
@@ -51,3 +51,68 @@ class Book(models.Model):
 		"""
 
 		return reverse('book-detail', args=[str(self.id)])
+
+import uuid # Required for unique book instances
+
+################
+# BookInstance 
+################
+class BookInstance(models.Model):
+	"""
+	Model representing a a specific copy of a book (i.e that can be borrowed from the library).
+	"""
+
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+	book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+	imprint = models.CharField(max_length=200)
+	due_back = models.DateField(null=True, blank=True)
+
+	LOAN_STATUS = (
+		('m', 'Maintenance'),
+		('o', 'On Loan'),
+		('a', 'Available'),
+		('r', 'Reserved'),
+	)
+
+	status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text="Book availability")
+
+	class Meta:
+		ordering = ["due_back"]
+
+	def __str__(self):
+		"""
+		String for representing the Model object
+		"""
+
+		return f'{self.id} ({self.book.title})'
+
+
+################
+# Author 
+################
+class Author(models.Model):
+	"""
+	Model representing an author.
+	"""
+
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	date_of_birth = models.DateField(null=True, blank=True)
+	date_of_death = models.DateField('Died', null=True, blank=True)
+
+	class Meta:
+		ordering = ["last_name", "first_name"]
+
+	def get_absolute_url(self):
+		"""
+		Returns the url to access a particular author instance.
+		"""
+
+		return reverse('author-detail', args=[str(self.id)])
+
+	def __str__(self):
+		"""
+		String for representing the Model object.
+		"""
+
+		return f'{self.last_name}, {self.first_name}'
