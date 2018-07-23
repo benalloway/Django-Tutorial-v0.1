6ed179c25e8f0590cbc0c8c487c5607c0c1f1a2a
benalloway <benalloway@gmail.com>
2018-07-23 08:36:44 -0400

    added Admin classes to each model, any changes to admin view: modify associated class.

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,12 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
 
+# Register your models here.
 from .models import Author, NaturalLanguage, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
-admin.site.register(NaturalLanguage)
 
+##################
+# Register Admin Pages for the following models
+# Decided to go with decorator registerations. For all desired changes to admin views, modify the associated class.
+##################
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(NaturalLanguage)
+class NaturalLanguageAdmin(admin.ModelAdmin):
+	pass
