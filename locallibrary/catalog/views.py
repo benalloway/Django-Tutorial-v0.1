b193bd9f4aa97bbbf22a2a2e90efc73513389fe1
benalloway <benalloway@gmail.com>
2018-07-24 14:05:07 -0400

    added functionality to search for books containing keywords in summary and then display the count of them

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,10 +12,18 @@ def index(request):
 	View functionf or home page of site
 	"""
 
+	# GET summary from front end form input to search DB for books containing key words within their summary.
+	try:
+		summary = request.GET["summary"]
+		num_books_summary = Book.objects.filter(summary__icontains=summary).count()
+	except:
+		summary = 0
+		num_books_summary = 0
 	# Generate counts of asome of the main objects
 	num_books = Book.objects.all().count()
 	num_instances = BookInstance.objects.all().count()
 	num_authors = Author.objects.count() # the .all() is implied by defult, example.
+	num_genres = Genre.objects.count()
 	# Available books (status = 'a')
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
@@ -23,5 +31,5 @@ def index(request):
 	return render(
 		request,
 		'index.html',
-		context = {'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors},
+		context = {'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors, 'num_genres':num_genres, 'num_books_summary':num_books_summary, 'summary_search':summary},
 	)
