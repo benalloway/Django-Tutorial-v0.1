117fa92568bde2236c9867fab53d8cbf360bdd3b
benalloway <benalloway@gmail.com>
2018-07-31 11:55:43 -0400

    updated views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -27,11 +27,23 @@ def index(request):
 	# Available books (status = 'a')
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
+	# Number of visits to this view, as counted in the session variable.
+	num_visits = request.session.get('num_visits', 1)
+	request.session['num_visits'] = num_visits + 1
+
 	# Render the HTML template index.html with the data in the context variable
 	return render(
 		request,
 		'index.html',
-		context = {'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors, 'num_genres':num_genres, 'num_books_summary':num_books_summary, 'summary_search':summary},
+		context = {
+		'num_books':num_books, 
+		'num_instances':num_instances, 
+		'num_instances_available':num_instances_available, 
+		'num_authors':num_authors, 
+		'num_genres':num_genres, 
+		'num_books_summary':num_books_summary, 
+		'summary_search':summary, 
+		'num_visits':num_visits,},
 	)
 
 # Class based view, using generic list view
